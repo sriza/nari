93008fc6de4f8bca11bac2cd32cc7612298a35e9
sriza <sriza054@mail.com>
2019-11-19 23:39:00 +0545

    authentication and user session added

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 const app = express();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+
+
+
+const {
+  ensureAuthenticated,
+  forwardAuthenticated
+} = require('../config/auth');
 
 //databases, Schema 
 var Gyno = require('../models/gyno_info');
-var User = require('../models/user');
 var Blog = require('../models/blog');
 var Period = require('../models/periods');
 var FPreg = require('../models/pregnancies');
@@ -14,124 +21,271 @@ var SPreg = require('../models/second_trimester')
 var Hbp = require('../models/highbp')
 var Weightl = require('../models/weight_loss')
 var Leg = require('../models/leg')
+var User = require('../models/user');
 
-/* Landing page */
-router.get('/', function (req, res, next) {
-  res.render('home', { title: 'NARI' });
+
+
+
+
+
+
+
+//landing page
+router.get('/', forwardAuthenticated, function (req, res, next) {
+  res.render('landingpage', {
+    title: 'NARI'
+  });
 });
 
 
+
+
 //signin page
-router.get('/signin', function (req, res, next) {
-  /*  var email = req.body.email;
-   var password = req.body.password;
- 
-   User.getUserByName(email)
-     .then(function (user) {
-       return bcrypt.compare(password, user.password);
-     })
-     .then(function (samePassword) {
-       if (!samePassword) {
-         res.status(403).send();
-       }
-       res.send();
-     })
-     .catch(function (error) {
-       console.log("Error authenticating user: ");
-       console.log(error);
-       next();
-     }); */
+router.get('/signin', forwardAuthenticated, async function (req, res, next) {
   res.render('signin');
+});
+
+/* router.post('/signin', function (req, res, next) {
+
+  
 })
+ */
+router.post('/signin',
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/signin',
+    failureFlash: true
+  })
+);
+
+
+
+//go home once authenticated
+
+
+//User Registration
+router.get('/signup', forwardAuthenticated, async function (req, res, next) {
+  res.render('signup');
+});
+
+/* router.post('/adduser', function (req, res, next) {
+  console.log(req.body);
+  User.find({
+      email: req.body.email
+    }).exec()
+    .then(user => {
+      if (user.length >= 1) {
+        console.log('User already exists');
+        res.redirect('/');
+      } else {
+        const user = new User({
+          email: req.body.email,
+          password: req.body.password,
+          age: req.body.age,
+          name: req.body.name,
+        });
+        user.save()
+          .then(result => {
+            console.log(result);
+            res.redirect('/');
+          }).catch(err => {
+            console.log(err);
+          });
+      }
+    })
+
+
+    .catch(function (error) {
+      console.log("Error authenticating user: User not found ");
+      console.log(error);
+      next();
+    });
+
+}); */
+
+router.post('/adduser', function (req, res, next) {
+  console.log(req.body);
+  User.find({
+      email: req.body.email
+    }).exec()
+    .then(user => {
+      if (user.length >= 1) {
+        console.log('User already exists');
+        res.redirect('/');
+      } else {
+
+        /*         bcrypt.genSalt(10, (err, salt) => {
+         */
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
+            });
+
+          } else {
+            const usere = new User({
+              email: req.body.email,
+              password: hash,
+              age: req.body.age,
+              username: req.body.name,
+            });
+            usere.save()
+              .then(result => {
+                console.log(result);
+                req.flash('success_msg', 'Your are now registered and can login');
+                res.redirect('/signin');
+              }).catch(err => {
+                console.log(err);
+              });
+          }
+        });
+        /*         })
+         */
+
+      }
+    })
+    .catch((err) => {
+      console.log('error.......' + err);
+    })
+
+});
+
+//logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/');
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//home
+router.get('/home', ensureAuthenticated, function (req, res, next) {
+  res.render('home', {
+    title: 'NARI',
+    user: req.user
+  });
+});
+
+
+
 
 
 //blog
 //blog page
-router.get('/blog', function (req, res, next) {
+router.get('/blog', ensureAuthenticated, function (req, res, next) {
   Blog.find().exec((err, blog) => {
-    res.render('blog', { blog });
+    res.render('blog', {
+      blog,
+      user: req.user
+    });
     console.log('....... data', blog);
     return blog;
-  }
-  )
+  })
 });
 
 //Information of gynologist
-router.get('/gyno', function (req, res, next) {
+router.get('/gyno', ensureAuthenticated, function (req, res, next) {
   Gyno.find().exec((err, gyno) => {
-    res.render('gyno', { gyno });
+    res.render('gyno', {
+      gyno
+    });
     console.log('....... data', gyno);
     return gyno;
-  }
-  )
+  })
 });
 
 //exercise
-router.get('/exercise', function (req, res, next) {
+router.get('/exercise', ensureAuthenticated, function (req, res, next) {
   res.render('exercise');
 })
 
 //period exercises
-router.get('/exercise/periods', function (req, res, next) {
+router.get('/exercise/periods', ensureAuthenticated, function (req, res, next) {
   Period.find().exec((err, period) => {
-    res.render('exerciseDesc', { period });
+    res.render('exerciseDesc', {
+      period
+    });
     console.log('....... data', period);
     return period;
-  }
-  )
+  })
 });
 
 //firsttrimester pregnancy
-router.get('/exercise/pregnancy/first', function (req, res, next) {
+router.get('/exercise/pregnancy/first', ensureAuthenticated, function (req, res, next) {
   FPreg.find().exec((err, period) => {
-    res.render('exerciseDesc', { period });
+    res.render('exerciseDesc', {
+      period
+    });
     console.log('....... data', period);
     return period;
-  }
-  )
+  })
 });
 
 
 //second trimester pregnancy
-router.get('/exercise/pregnancy/second', function (req, res, next) {
+router.get('/exercise/pregnancy/second', ensureAuthenticated, function (req, res, next) {
   SPreg.find().exec((err, period) => {
-    res.render('exerciseDesc', { period });
+    res.render('exerciseDesc', {
+      period
+    });
     console.log('....... data', period);
     return period;
-  }
-  )
+  })
 });
 
 
 //high bp
-router.get('/exercise/bp', function (req, res, next) {
+router.get('/exercise/bp', ensureAuthenticated, function (req, res, next) {
   Hbp.find().exec((err, period) => {
-    res.render('exerciseDesc', { period });
+    res.render('exerciseDesc', {
+      period
+    });
     console.log('....... data', period);
     return period;
-  }
-  )
+  })
 });
 
 
 //legs
-router.get('/exercise/leg', function (req, res, next) {
+router.get('/exercise/leg', ensureAuthenticated, function (req, res, next) {
   Leg.find().exec((err, period) => {
-    res.render('exerciseDesc', { period });
+    res.render('exerciseDesc', {
+      period
+    });
     console.log('....... data', period);
     return period;
-  }
-  )
+  })
 });
 
 //weight loss
-router.get('/exercise/weightloss', function (req, res, next) {
+router.get('/exercise/weightloss', ensureAuthenticated, function (req, res, next) {
   Weightl.find().exec((err, period) => {
-    res.render('exerciseDesc', { period });
+    res.render('exerciseDesc', {
+      period
+    });
     console.log('....... data', period);
     return period;
-  }
-  )
+  })
 });
 
 
@@ -144,7 +298,7 @@ router.get('/exercise/weightloss', function (req, res, next) {
 
 //Form of blog
 
-router.get('/blogform', function (req, res, next) {
+router.get('/blogform', ensureAuthenticated, function (req, res, next) {
   res.render('blog_form');
 });
 
@@ -169,18 +323,24 @@ router.post('/addblog', function (req, res, next) {
 
 
 //one blog
-router.get('/blog/:_id', function (req, res, next) {
+router.get('/blog/:_id', ensureAuthenticated, function (req, res, next) {
 
-  Blog.findOne({ _id: req.params._id }, function (err, blog) {
-    res.render('blogone', { blog });
+  Blog.findOne({
+    _id: req.params._id
+  }, function (err, blog) {
+    res.render('blogone', {
+      blog
+    });
     console.log('blog selected .....', blog);
   })
 });
 
 
 //delete blog
-router.get('/delete/:_id', function (req, res, next) {
-  Blog.deleteOne({ _id: req.params._id }, function (err, movie) {
+router.get('/delete/:_id', ensureAuthenticated, function (req, res, next) {
+  Blog.deleteOne({
+    _id: req.params._id
+  }, function (err, movie) {
     console.log('blog deleted.....', blog);
     res.redirect('/blog');
   });
@@ -189,59 +349,16 @@ router.get('/delete/:_id', function (req, res, next) {
 
 
 //edit blog
-router.get('/edit/:_id', function (req, res, next) {
-  Blog.findOne({ _id: req.params._id }, function (err, movie) {
+router.get('/edit/:_id', ensureAuthenticated, function (req, res, next) {
+  Blog.findOne({
+    _id: req.params._id
+  }, function (err, movie) {
     console.log('blog selected....', blog);
-    res.render('updateMovie', { blog });
+    res.render('updateMovie', {
+      blog
+    });
   });
 })
-
-
-
-//User Registration
-router.get('/signup', async function (req, res, next) {
-  res.render('signup');
-});
-
-router.post('/adduser', function (req, res, next) {
-  console.log(req.body);
-  User.find({ email: req.body.email }).exec()
-    .then(user => {
-      if (user.length >= 1) {
-        console.log('User already exists');
-        res.redirect('/');
-      } else {
-        bcrypt.hash(req.body.email, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json(
-              {
-                error: err
-              });
-
-          } else {
-            const user = new User({
-              email: req.body.email,
-              password: hash,
-              age: req.body.age,
-              name: req.body.name,
-            });
-            user.save()
-              .then(result => {
-                console.log(result);
-                res.redirect('/');
-              }).catch(err => {
-                console.log(err);
-              });
-          }
-        });
-
-      }
-    })
-    .catch((err) => {
-      console.log('error.......' + err);
-    })
-
-});
 
 
 module.exports = router;
